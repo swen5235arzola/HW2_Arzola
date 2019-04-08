4a546b1a91c2c2fd62b5e00f7fbb8d5ca05ed81d
adrycontreras23 <adrycontreras23@gmail.com>
2019-04-08 11:29:12 -0500

    Update GameOverManager.cs
    apply coding practices

--- Assets/Scripts/Managers/GameOverManager.cs
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -1,18 +1,18 @@
 ﻿using UnityEngine;
 
+//Author: Unity Tutorial
+//Modified By: Adriana Arzola
+//Date: 04/07/2019
 public class GameOverManager : MonoBehaviour
 {
     public PlayerHealth playerHealth;
 
-
     Animator anim;
-
 
     void Awake()
     {
         anim = GetComponent<Animator>();
     }
-
 
     void Update()
     {
