3cd6f0e1b78d4cb59298f4977b4b5c5207d83aaa
Adriana Arzola <adycontreras23@gmail.com>
2019-04-08 00:45:23 -0500

    completed zombie game

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing = 5f;
+
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 targetCamPostion = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPostion, smoothing * Time.deltaTime);
+    }
+}
