d0528a65eb1347d66ce2974c1c1494a147e566b5
adrycontreras23 <adrycontreras23@gmail.com>
2019-04-08 11:30:28 -0500

    Update ScoreManager.cs
    apply coding practices

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -2,13 +2,14 @@
 using UnityEngine.UI;
 using System.Collections;
 
+//Author: Unity Tutorial
+//Modified By: Adriana Arzola
+//Date: 04/07/2019
 public class ScoreManager : MonoBehaviour
 {
     public static int score;
 
-
     Text text;
-
 
     void Awake ()
     {
